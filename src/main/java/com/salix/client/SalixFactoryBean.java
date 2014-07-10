e8de29f8c85d492b4ed5da36a62405342df4cab1
duanbn <duanbn@kaolafm.com>
2014-07-10 11:19:31 +0800

    coding

--- src/main/java/com/salix/client/SalixFactoryBean.java
+++ src/main/java/com/salix/client/SalixFactoryBean.java
@@ -2,10 +2,16 @@ package com.salix.client;
 
 import java.lang.reflect.Proxy;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.InitializingBean;
+
 import com.salix.client.connection.ConnectionPool;
 import com.salix.client.connection.MyConnectionPool;
 
-public class SalixFactoryBean {
+public class SalixFactoryBean<T> implements FactoryBean<T>, InitializingBean, DisposableBean {
+
+	private String id;
 
 	private ConnectionPool cpool;
 
@@ -13,20 +19,46 @@ public class SalixFactoryBean {
 
 	private int port;
 
-	public Object create(String serviceName, String interfaceClass) throws ClassNotFoundException {
+	private String serviceName;
+
+	private String interfaceClass;
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public T getObject() throws Exception {
 		ClassLoader cl = Thread.currentThread().getContextClassLoader();
 		Class<?> clazz = Class.forName(interfaceClass);
 		RpcInvocationHandler rpcInvokeHandler = new RpcInvocationHandler(serviceName, this.cpool);
-		return Proxy.newProxyInstance(cl, new Class<?>[] { clazz }, rpcInvokeHandler);
+		return (T) Proxy.newProxyInstance(cl, new Class<?>[] { clazz }, rpcInvokeHandler);
 	}
 
-	public void startup() {
+	@Override
+	public Class<?> getObjectType() {
+		return this.getClass();
+	}
+
+	@Override
+	public boolean isSingleton() {
+		return true;
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
 		this.cpool = new MyConnectionPool(host, port);
 		this.cpool.startup();
 	}
 
-	public void shutdown() {
+	@Override
+	public void destroy() throws Exception {
 		this.cpool.shutdown();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 
 	public String getHost() {
@@ -43,6 +75,22 @@ public class SalixFactoryBean {
 
 	public void setPort(int port) {
 		this.port = port;
+	}
+
+	public String getServiceName() {
+		return serviceName;
+	}
+
+	public void setServiceName(String serviceName) {
+		this.serviceName = serviceName;
+	}
+
+	public String getInterfaceClass() {
+		return interfaceClass;
+	}
+
+	public void setInterfaceClass(String interfaceClass) {
+		this.interfaceClass = interfaceClass;
 	}
 
 }
